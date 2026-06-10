7cee03f0c6e9c1010873f0f0eb3b87bf1b5e1228
Nashap <nashanoushad19@gmail.com>
2026-06-10 14:17:00 +0530

    Feature 2: Storage integration, upload API, pagination and filtering

--- care_ride/settings.py
+++ care_ride/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+    'django_filters',
 
     'users',
     'helpers',
@@ -140,4 +141,13 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
+
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend',
+    ],
+
+    'DEFAULT_PAGINATION_CLASS':
+        'rest_framework.pagination.PageNumberPagination',
+
+    'PAGE_SIZE': 5,
 }
--- rides/views.py
+++ rides/views.py
@@ -1,9 +1,22 @@
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from django_filters.rest_framework import DjangoFilterBackend
+
 from .models import TravelRequest
 from .serializers import TravelRequestSerializer
-from rest_framework.permissions import IsAuthenticated
+
 
 class TravelRequestViewSet(viewsets.ModelViewSet):
     queryset = TravelRequest.objects.all()
     serializer_class = TravelRequestSerializer
+
     permission_classes = [IsAuthenticated]
+
+    filter_backends = [DjangoFilterBackend]
+
+    filterset_fields = [
+        'service_type',
+        'assistance_type',
+        'assistance_level',
+        'status'
+    ]
