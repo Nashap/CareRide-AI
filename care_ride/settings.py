e2d1457554d87297b4af39af26f27265fcaf60b6
Nashap <nashanoushad19@gmail.com>
2026-06-11 15:26:09 +0530

    fix: add default SECRET_KEY for GitHub Actions

--- care_ride/settings.py
+++ care_ride/settings.py
@@ -29,7 +29,10 @@ environ.Env.read_env(BASE_DIR / ".env")
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = env(
+    'SECRET_KEY',
+    default='github-actions-secret-key'
+    )
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool('DEBUG', default=True)
