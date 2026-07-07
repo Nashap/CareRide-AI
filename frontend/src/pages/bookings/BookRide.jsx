10543ead71f432894a9ddd7eec3dd3e7137736e5
Nasha p <119646624+Nashap@users.noreply.github.com>
2026-07-07 22:06:30 +0530

    Update BookRide.jsx

--- frontend/src/pages/bookings/BookRide.jsx
+++ frontend/src/pages/bookings/BookRide.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
+import { Loader2 } from "lucide-react";
 
 import RiderNavbar from "../../components/dashboard/RiderNavbar";
 import RiderSidebar from "../../components/dashboard/RiderSidebar";
